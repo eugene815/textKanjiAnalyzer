d91b1cc3147d66fc3ac6c570e1dc6fae4d5c718c
Evgeny Us <usupovzh@live.com>
2017-06-27 11:05:42 +0300

    Add parsers and data samples

---
+++ Parser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataProcessingTemplate
+{
+    class Parser
+    {
+        public static Dictionary<string, string> parseKore(string kore)
+        {
+
+            Dictionary<string, string> words = new Dictionary<string, string>();
+
+            string[] wordsRaw = kore.Split(new string[] { "Core" }, StringSplitOptions.None);
+
+            int count = 0;
+            foreach (string wordString in wordsRaw)
+            {
+                // borderlane cases
+                if (count == 0 || count >= 5999)
+                {
+                    count++;
+                    continue;
+                }
+
+                string[] wordData = wordString.Split('\t');
+
+                string japaneseWord = wordData[3].Split('\n')[1];
+
+                if (!words.ContainsKey(japaneseWord))
+                    words.Add(japaneseWord, wordData[6]);
+
+                count++;
+            }
+
+            return words;
+        }
+
+        public static Dictionary<string, string> parseKore10k(string kore)
+        {
+
+            Dictionary<string, string> words = new Dictionary<string, string>();
+
+            string[] wordsRaw = kore.Split('\t');
+
+            for (int i = 1; i < wordsRaw.Length / 14; i++)
+            {
+
+                string japWord = wordsRaw[3 + i * 14];
+                string translation = wordsRaw[5 + i * 14];
+
+                if (!words.ContainsKey(japWord))
+                    words.Add(japWord, translation);
+            }
+
+            return words;
+        }
+
+        public static Dictionary<string, int> parseFreqList(string freqList)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            string[] wordsFreqRaw = freqList.Split('\n');
+
+            foreach (string wordRaw in wordsFreqRaw)
+            {
+                string[] word = wordRaw.Split('\t');
+                if (!words.ContainsKey(word[2]))
+                    words.Add(word[2], Convert.ToInt32(word[0]));
+            }
+
+            return words;
+        }
+    }
+}
--- Utils.cs
+++ Utils.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 namespace DataProcessingTemplate
 {
     static class Utils
     {
-        public static string getTextFileContent (string filePath) {
+        public static string getTextFileContent(string filePath)
+        {
             if (File.Exists(filePath))
             {
                 return File.ReadAllText(filePath, Encoding.Unicode);
@@ -16,6 +18,23 @@ namespace DataProcessingTemplate
                 Console.WriteLine("File not found...");
                 return "";
             }
+        }
+
+        public static void writeStringToFile(string data)
+        {
+            StreamWriter file = new System.IO.StreamWriter("result.txt");
+            file.WriteLine(data);
+            file.Close();
+        }
+
+        public static string formatKanjiFile()
+        {
+            string result = "";
+            string filePath = "kanjiRaw.txt";
+
+            result = Utils.getTextFileContent(filePath).Replace(" ", "', '");
+
+            return result;
         }
     }
 }
